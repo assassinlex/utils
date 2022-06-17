59bfc44d710227bbe5606c4bbb9386c6e8623ca1
lexgod <lexluowork@gmail.com>
2022-06-17 18:26:47 +0800

    health -> Health

--- net/hander.go
+++ net/hander.go
@@ -2,7 +2,7 @@ package net
 
 import "net/http"
 
-// health 健康检查 handler
-func health(w http.ResponseWriter, r *http.Request)  {
+// Health  健康检查 handler
+func Health(w http.ResponseWriter, r *http.Request)  {
 	_, _ = w.Write([]byte(""))
 }
